0d2790cc7de838d33456045fe7650c208ca8976b
Alan <aaltamira@actinver.com.mx>
2022-11-08 13:35:37 -0600

    se agrega ViewBinding

--- app/src/main/java/com/example/kotlin_examples/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_examples/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.kotlin_examples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin_examples.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setUpView()
     }
+
+    /** */
+    private fun setUpView() {
+        binding.tilEmail.editText?.setText("Test")
+    }
+
+
 }
